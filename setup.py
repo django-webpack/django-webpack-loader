0454aae2e05b14c9818a947d6d04ca65f542894f
Owais Lone <hello@owaislone.org>
2015-05-16 22:48:26 +0530

    Added github release url to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
   description = 'Load your webpack bundles and chunks in django',
   author = 'Owais Lone',
   author_email = 'hello@owaislone.org',
+  download_url = 'https://github.com/owais/django-webpack-loader/tarball/0.0.1',
   url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
   keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
   classifiers = [],
