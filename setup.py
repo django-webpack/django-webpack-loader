d1aec668df57065196029f8184ff6800e1bb4c55
Owais Lone <hello@owaislone.org>
2015-05-21 22:36:41 +0530

    Added license to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from distutils.core import setup
 setup(
   name = 'django-webpack-loader',
   packages = ['webpack_loader', 'webpack_loader/templatetags'], # this must be the same as the name above
-  version = '0.0.4.1',
+  version = '0.0.4.2',
   description = 'Load your webpack bundles and chunks in django',
   author = 'Owais Lone',
   author_email = 'hello@owaislone.org',
   download_url = 'https://github.com/owais/django-webpack-loader/tarball/0.0.3',
   url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
   keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
-  classifiers = [],
+  data_files = [("", ["LICENSE"])],
+  classifiers = [
+    'Programming Language :: Python :: 2.7',
+    'Framework :: Django',
+    'Environment :: Web Environment',
+    'License :: OSI Approved :: MIT License',
+  ],
 )
