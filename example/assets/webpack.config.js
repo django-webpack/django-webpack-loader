3cd536d6451730ac53271ce4e4f9f37ff5135c18
Owais Lone <hello@owaislone.org>
2015-05-23 13:06:27 +0530

    Updated example

--- example/assets/webpack.config.js
+++ example/assets/webpack.config.js
@@ -5,21 +5,13 @@ var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
   context: __dirname,
-  entry: [
-      'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
-      './js/index'
-  ],
-  
+  entry: './js/index',
   output: {
       path: path.resolve('./assets/bundles/'),
       filename: "[name]-[hash].js",
-      publicPath: 'http://localhost:3000/assets/bundles/', // to serve our bundles from webpack-dev-server instead of django
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(), // don't reload if there is an error
     new BundleTracker({filename: './assets/webpack-stats.json'}),
   ],
 
