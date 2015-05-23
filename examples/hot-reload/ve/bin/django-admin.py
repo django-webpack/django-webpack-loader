b4e5cb5a3394ec0762d7090b1201196aa91d6d9e
Owais Lone <hello@owaislone.org>
2015-05-23 14:41:24 +0530

    New Release Removed ve, node_modules, bundles form examples

--- examples/hot-reload/ve/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/owais/Projects/django-webpack-loader/examples/hot-reload/ve/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
