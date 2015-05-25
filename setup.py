a3c5bc95df76fc415948bf99a6bdfa73f7ec30c5
Owais Lone <hello@owaislone.org>
2015-05-26 01:34:22 +0530

    Bumped version.. again.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from distutils.core import setup
+
+version = '0.1.2'
+
 setup(
   name = 'django-webpack-loader',
   packages = ['webpack_loader', 'webpack_loader/templatetags'], # this must be the same as the name above
@@ -6,7 +9,7 @@ setup(
   description = 'Load your webpack bundles and chunks in django',
   author = 'Owais Lone',
   author_email = 'hello@owaislone.org',
-  download_url = 'https://github.com/owais/django-webpack-loader/tarball/0.0.3',
+  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{}'.format(version),
   url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
   keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
   data_files = [("", ["LICENSE"])],
