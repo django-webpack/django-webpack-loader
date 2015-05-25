9b8dbd852a0807e93fc8dfa0da31371e808e22ca
Owais Lone <hello@owaislone.org>
2015-05-25 23:49:20 +0530

    Added more test cases

---
+++ tests/assets/js/bad_index.jsx
@@ -0,0 +1,5 @@
+var React = require('the-library-that-did-not-exist');
+var App = require('./app');
+var style = require('./style.css');
+
+React.render(<App/>, document.getElementById('react-app'));
