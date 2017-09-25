323839c49532ed0f9d7cad5c8abed3024baaeaf4
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-09-25 14:11:28 -0700

    Fix for No 'Access-Control-Allow-Origin' header is present error. (#130)

--- examples/hot-reload/server.js
+++ examples/hot-reload/server.js
@@ -6,7 +6,8 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   inline: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  headers: { 'Access-Control-Allow-Origin': '*' }
 }).listen(3000, '0.0.0.0', function (err, result) {
   if (err) {
     console.log(err);
