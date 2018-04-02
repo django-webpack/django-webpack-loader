a40ab87e7366112d84b347943ab143a78f157a78
Philip Marais <pmarais@users.noreply.github.com>
2018-04-02 15:03:33 +0200

    Update urls.py to avoid namespace error (#156)
    Updating the simple/app/urls.py to avoid `django.core.exceptions.ImproperlyConfigured` error.

--- examples/simple/app/urls.py
+++ examples/simple/app/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-
-
+## For Django 2+ support
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls), ## Django 2+ admin namespace
 ]
