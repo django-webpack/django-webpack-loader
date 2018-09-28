c12f42c9d73ad9a408093bf15138a3104925cfa5
Nikolaus Piccolotto <nikolaus.piccolotto@artory.com>
2018-09-28 18:37:17 +0200

    Remove code splitting test and examples

--- examples/code-splitting/assets/js/app.jsx
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-
-module.exports = React.createClass({
-   render: function(){
-       return <h1>Hello, world.</h1>
-   }
-});
--- examples/code-splitting/assets/js/index.jsx
+++
@@ -1,4 +0,0 @@
-var React = require('react');
-var App = require('./app');
-
-React.render(<App/>, document.getElementById('react-app'));
