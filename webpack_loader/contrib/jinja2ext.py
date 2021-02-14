b489d060bdb905477a64488caeed408b16dbbb56
MrP01 <peter@waldert.at>
2021-02-14 17:20:40 +0100

    introduce additional templatetags register_entrypoint, render_css, render_js, inline_entrypoint

--- webpack_loader/contrib/jinja2ext.py
+++ webpack_loader/contrib/jinja2ext.py
@@ -1,6 +1,6 @@
 import jinja2.ext
 
-from ..templatetags.webpack_loader import render_bundle, render_entrypoint
+from ..templatetags.webpack_loader import *
 
 
 class WebpackExtension(jinja2.ext.Extension):
@@ -8,3 +8,6 @@ class WebpackExtension(jinja2.ext.Extension):
         super(WebpackExtension, self).__init__(environment)
         environment.globals["render_bundle"] = lambda *a, **k: jinja2.Markup(render_bundle(*a, **k))
         environment.globals["render_entrypoint"] = lambda *a, **k: jinja2.Markup(render_entrypoint(*a, **k))
+
+    def parse(self, parser):
+        super(WebpackExtension, self).parse(parser)
