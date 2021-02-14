f4b28aa24dab186d6a53028fe22809cf52bf77ae
MrP01 <peter@waldert.at>
2021-02-14 12:09:17 +0100

    add django31.txt requirements file, further clean up

--- webpack_loader/utils.py
+++ webpack_loader/utils.py
@@ -9,7 +9,7 @@ _loaders = {}
 def get_loader(config_name):
     if config_name not in _loaders:
         config = load_config(config_name)
-        loader_class = import_string(config['LOADER_CLASS'])
+        loader_class = import_string(config["LOADER_CLASS"])
         _loaders[config_name] = loader_class(config_name, config)
     return _loaders[config_name]
 
@@ -17,7 +17,7 @@ def get_loader(config_name):
 def _filter_by_extension(bundle, extension):
     """Return only files with the given extension"""
     for chunk in bundle:
-        if chunk['name'].endswith('.{0}'.format(extension)):
+        if chunk["name"].endswith(".{0}".format(extension)):
             yield chunk
 
 
@@ -28,39 +28,40 @@ def _get_bundle(bundle_name, extension, config):
     return bundle
 
 
-def get_files(bundle_name, extension=None, config='DEFAULT'):
+def get_files(bundle_name, extension=None, config="DEFAULT"):
     """Returns list of chunks from named bundle"""
     return list(_get_bundle(bundle_name, extension, config))
 
 
-def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
+def get_as_tags(bundle_name, extension=None, config="DEFAULT", attrs=""):
     """
     Get a list of formatted <script> & <link> tags for the assets in the
     named bundle.
 
     :param bundle_name: The name of the bundle
-    :param extension: (optional) filter by extension, eg. 'js' or 'css'
+    :param extension: (optional) filter by extension, eg. "js" or "css"
     :param config: (optional) the name of the configuration
+    :param attrs: (optional) further attributes on the tags
     :return: a list of formatted tags as strings
     """
 
     bundle = _get_bundle(bundle_name, extension, config)
     tags = []
     for chunk in bundle:
-        if chunk['name'].endswith(('.js', '.js.gz')):
+        if chunk["name"].endswith((".js", ".js.gz")):
             tags.append((
                 '<script type="text/javascript" src="{0}" {1}></script>'
-            ).format(chunk['url'], attrs))
-        elif chunk['name'].endswith(('.css', '.css.gz')):
+            ).format(chunk["url"], attrs))
+        elif chunk["name"].endswith((".css", ".css.gz")):
             tags.append((
                 '<link type="text/css" href="{0}" rel="stylesheet" {1}/>'
-            ).format(chunk['url'], attrs))
+            ).format(chunk["url"], attrs))
     return tags
 
 
-def get_static(asset_name, config='DEFAULT'):
+def get_static(asset_name, config="DEFAULT"):
     """
-    Equivalent to Django's 'static' look up but for webpack assets.
+    Equivalent to Django"s "static" look up but for webpack assets.
 
     :param asset_name: the name of the asset
     :param config: (optional) the name of the configuration
@@ -68,7 +69,7 @@ def get_static(asset_name, config='DEFAULT'):
     """
     return "{0}{1}".format(
         get_loader(config).get_assets().get(
-            'publicPath', getattr(settings, 'STATIC_URL')
+            "publicPath", getattr(settings, "STATIC_URL")
         ),
         asset_name
     )
