029363b9562b5e559df4f12ab6a812db310ffd04
MrP01 <peter@waldert.at>
2021-02-14 12:05:46 +0100

    reformat utils.py, setup.py; upgrade requirements-dev.txt to the latest versions

--- setup.py
+++ setup.py
@@ -5,37 +5,37 @@ from setuptools import setup
 
 
 def rel(*parts):
-    '''returns the relative path to a file wrt to the current directory'''
+    """returns the relative path to a file wrt to the current directory"""
     return os.path.abspath(os.path.join(os.path.dirname(__file__), *parts))
 
-README = open('README.md', 'r').read()
 
-with open(rel('webpack_loader', '__init__.py')) as handler:
+README = open("README.md", "r").read()
+
+with open(rel("webpack_loader", "__init__.py")) as handler:
     INIT_PY = handler.read()
-
 
 VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
 
 setup(
-  name = 'django-webpack-loader',
-  packages = ['webpack_loader', 'webpack_loader/templatetags', 'webpack_loader/contrib'],
-  version = VERSION,
-  description = 'Transparently use webpack with django',
-  long_description=README,
-  long_description_content_type="text/markdown",
-  author = 'Owais Lone',
-  author_email = 'hello@owaislone.org',
-  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{0}'.format(VERSION),
-  url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
-  keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
-  classifiers = [
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Framework :: Django',
-    'Environment :: Web Environment',
-    'License :: OSI Approved :: MIT License',
-  ],
+    name="django-webpack-loader",
+    packages=["webpack_loader", "webpack_loader/templatetags", "webpack_loader/contrib"],
+    version=VERSION,
+    description="Transparently use webpack with django",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Owais Lone",
+    author_email="hello@owaislone.org",
+    download_url="https://github.com/owais/django-webpack-loader/tarball/{0}".format(VERSION),
+    url="https://github.com/owais/django-webpack-loader",  # use the URL to the github repo
+    keywords=["django", "webpack", "assets"],  # arbitrary keywords
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
--- webpack_loader/utils.py
+++ webpack_loader/utils.py
@@ -1,24 +1,9 @@
-from importlib import import_module
 from django.conf import settings
+from django.utils.module_loading import import_string
+
 from .config import load_config
 
-
 _loaders = {}
-
-
-def import_string(dotted_path):
-    '''
-    This is a rough copy of django's import_string, which wasn't introduced until Django 1.7
-
-    Once this package's support for Django 1.6 has been removed, this can be safely replaced with
-    `from django.utils.module_loading import import_string`
-    '''
-    try:
-        module_path, class_name = dotted_path.rsplit('.', 1)
-        module = import_module(module_path)
-        return getattr(module, class_name)
-    except (ValueError, AttributeError, ImportError):
-        raise ImportError('%s doesn\'t look like a valid module path' % dotted_path)
 
 
 def get_loader(config_name):
@@ -30,7 +15,7 @@ def get_loader(config_name):
 
 
 def _filter_by_extension(bundle, extension):
-    '''Return only files with the given extension'''
+    """Return only files with the given extension"""
     for chunk in bundle:
         if chunk['name'].endswith('.{0}'.format(extension)):
             yield chunk
@@ -44,12 +29,12 @@ def _get_bundle(bundle_name, extension, config):
 
 
 def get_files(bundle_name, extension=None, config='DEFAULT'):
-    '''Returns list of chunks from named bundle'''
+    """Returns list of chunks from named bundle"""
     return list(_get_bundle(bundle_name, extension, config))
 
 
 def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
-    '''
+    """
     Get a list of formatted <script> & <link> tags for the assets in the
     named bundle.
 
@@ -57,7 +42,7 @@ def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
     :param extension: (optional) filter by extension, eg. 'js' or 'css'
     :param config: (optional) the name of the configuration
     :return: a list of formatted tags as strings
-    '''
+    """
 
     bundle = _get_bundle(bundle_name, extension, config)
     tags = []
@@ -74,13 +59,13 @@ def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
 
 
 def get_static(asset_name, config='DEFAULT'):
-    '''
+    """
     Equivalent to Django's 'static' look up but for webpack assets.
 
     :param asset_name: the name of the asset
     :param config: (optional) the name of the configuration
     :return: path to webpack asset as a string
-    '''
+    """
     return "{0}{1}".format(
         get_loader(config).get_assets().get(
             'publicPath', getattr(settings, 'STATIC_URL')
