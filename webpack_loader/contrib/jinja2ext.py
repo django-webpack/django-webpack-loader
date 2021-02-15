a1fa7f6772daed8d820ff5d6b020b0e9428b406e
MrP01 <peter@waldert.at>
2021-02-15 14:20:21 +0100

    adapt jinja2ext to include the new templatetags

--- webpack_loader/contrib/jinja2ext.py
+++ webpack_loader/contrib/jinja2ext.py
@@ -1,13 +1,21 @@
 import jinja2.ext
 
 from ..templatetags.webpack_loader import *
+from ..contrib.pages.templatetags.webpack_pages import *
 
 
 class WebpackExtension(jinja2.ext.Extension):
     def __init__(self, environment):
         super(WebpackExtension, self).__init__(environment)
-        environment.globals["render_bundle"] = lambda *a, **k: jinja2.Markup(render_bundle(*a, **k))
-        environment.globals["render_entrypoint"] = lambda *a, **k: jinja2.Markup(render_entrypoint(*a, **k))
+        environment.globals.update({
+            "render_bundle": render_bundle,
+            "render_entrypoint": render_entrypoint,
+            "register_entrypoint": jinja2.contextfunction(register_entrypoint),
+            "render_css": jinja2.contextfunction(render_css),
+            "render_js": jinja2.contextfunction(render_js),
+            "inline_entrypoint": inline_entrypoint,
+            "asset": jinja2.contextfunction(asset_url),
+        })
 
     def parse(self, parser):
         super(WebpackExtension, self).parse(parser)
