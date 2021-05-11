d05f1f547c3ce27fdad284f5bce61f43c53f80ec
rvlb <renatovlbarros19@gmail.com>
2021-05-11 16:21:09 -0300

    Update hot-reload example

--- examples/hot-reload/assets/js/index.jsx
+++ examples/hot-reload/assets/js/index.jsx
@@ -1,4 +1,6 @@
-var React = require('react');
-var App = require('./app');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-React.render(<App/>, document.getElementById('react-app'));
+import App from './app';
+
+ReactDOM.render(<App/>, document.getElementById('react-app'));
