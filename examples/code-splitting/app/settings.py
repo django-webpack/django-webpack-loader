1762507e27a43672969d6f6463b8b0419b27c3e8
Renato Vieira <renatovlbarros19@gmail.com>
2021-05-13 17:02:45 -0300

    Update examples to use webpack-bundle-tracker@1.0.0 (#271)
    * Update examples to use webpack-bundle-tracker@1.0.0
    
    * Update readme with migration instructions

--- examples/code-splitting/app/settings.py
+++ examples/code-splitting/app/settings.py
@@ -109,7 +109,7 @@ STATICFILES_DIRS = (
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'assets/bundles/webpack-stats.json')
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json')
     }
 }
 
