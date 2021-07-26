066798db09c1d9cc5df5d88f403770ffb862f5e5
Alexandr Artemyev <mogost@gmail.com>
2021-07-27 00:03:19 +0300

    Add missed import

--- webpack_loader/__init__.py
+++ webpack_loader/__init__.py
@@ -1,5 +1,7 @@
 __author__ = 'Owais Lone'
 __version__ = '1.1.0'
 
+import django
+
 if django.VERSION < (3, 2):  # pragma: no cover
     default_app_config = 'webpack_loader.apps.WebpackLoaderConfig'
