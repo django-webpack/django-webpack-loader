3f226d0a6096a26e47ccb612c90f87736558bd1a
László Károlyi <laszlo@karolyi.hu>
2021-09-04 21:37:14 +0200

    Cleaning up tests, adding Jinja tests for skip_common_chunks

--- webpack_loader/contrib/jinja2ext.py
+++ webpack_loader/contrib/jinja2ext.py
@@ -1,10 +1,16 @@
-import jinja2.ext
+from jinja2.ext import Extension
+from jinja2.runtime import Context
+from jinja2.utils import pass_context
 
 from ..templatetags.webpack_loader import render_bundle
 
 
-class WebpackExtension(jinja2.ext.Extension):
+@pass_context
+def _render_bundle(context: Context, *args, **kwargs):
+    return render_bundle(context, *args, **kwargs)
+
+
+class WebpackExtension(Extension):
     def __init__(self, environment):
         super(WebpackExtension, self).__init__(environment)
-        environment.globals["context"] = dict()
-        environment.globals["render_bundle"] = lambda *a, **k: jinja2.Markup(render_bundle(environment.globals["context"], *a, **k))
+        environment.globals["render_bundle"] = _render_bundle
--- webpack_loader/templatetags/webpack_loader.py
+++ webpack_loader/templatetags/webpack_loader.py
@@ -1,36 +1,55 @@
-from django import template, VERSION
 from django.conf import settings
+from django.template import Library
 from django.utils.safestring import mark_safe
 
 from .. import utils
 
-register = template.Library()
+register = Library()
+_PROC_DJTEMPLATE = 'django.template.context_processors.request'
+_DJ_TEMPLATEPROCESSOR = 'django.template.backends.django.DjangoTemplates'
+_STARTUP_ERROR = (
+    f'Please make sure that "{_PROC_DJTEMPLATE}" is added '
+    'to your ["OPTIONS"]["context_processors"] list in your '
+    f'settings.TEMPLATES where the BACKEND is "{_DJ_TEMPLATEPROCESSOR}". '
+    'django-webpack-loader needs it and cannot run without it.')
+
+
+def _is_request_in_context():
+    for item in settings.TEMPLATES:
+        backend = item.get('BACKEND', {})
+        if backend == _DJ_TEMPLATEPROCESSOR:
+            processors = set(
+                item.get('OPTIONS', {}).get('context_processors', []))
+            if _PROC_DJTEMPLATE not in processors:
+                raise RuntimeError(_STARTUP_ERROR)
+
+
+# Check settings at module import time
+_is_request_in_context()
 
 
 @register.simple_tag(takes_context=True)
-def render_bundle(context, bundle_name, extension=None, config='DEFAULT', suffix='', attrs='', is_preload=False, skip_common_chunks=False):
+def render_bundle(
+        context, bundle_name, extension=None, config='DEFAULT', suffix='',
+        attrs='', is_preload=False, skip_common_chunks=False):
     tags = utils.get_as_tags(
-        bundle_name, extension=extension, config=config,
-        suffix=suffix, attrs=attrs, is_preload=is_preload
-    )
-    if "webpack_loader_used_tags" not in context:
-        context["webpack_loader_used_tags"] = set()
-    used_tags = context["webpack_loader_used_tags"]
+        bundle_name, extension=extension, config=config, suffix=suffix,
+        attrs=attrs, is_preload=is_preload)
+    if not hasattr(context['request'], '_webpack_loader_used_tags'):
+        context['request']._webpack_loader_used_tags = set()
+    used_tags = context['request']._webpack_loader_used_tags
     if skip_common_chunks:
         tags = [tag for tag in tags if tag not in used_tags]
-    context["webpack_loader_used_tags"].update(tags)
-
+    used_tags.update(tags)
     return mark_safe('\n'.join(tags))
+
 
 @register.simple_tag
 def webpack_static(asset_name, config='DEFAULT'):
     return utils.get_static(asset_name, config=config)
 
 
-assignment_tag = register.simple_tag if VERSION >= (1, 9) else register.assignment_tag
-
-
-@assignment_tag
+@register.simple_tag
 def get_files(bundle_name, extension=None, config='DEFAULT'):
     """
     Returns all chunks in the given bundle.
