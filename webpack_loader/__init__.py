740091c48043a06b2f8a9f5bf8e1e48c32ac31d9
Carlos Coelho <carlos@vinta.com.br>
2022-03-25 17:24:55 -0300

    Bump version to 1.5.0
    Signed-off-by: Carlos Coelho <carlos@vinta.com.br>

--- webpack_loader/__init__.py
+++ webpack_loader/__init__.py
@@ -1,7 +1,7 @@
-__author__ = 'Owais Lone'
-__version__ = '1.4.1'
+__author__ = "Owais Lone"
+__version__ = "1.5.0"
 
 import django
 
 if django.VERSION < (3, 2):  # pragma: no cover
-    default_app_config = 'webpack_loader.apps.WebpackLoaderConfig'
+    default_app_config = "webpack_loader.apps.WebpackLoaderConfig"
