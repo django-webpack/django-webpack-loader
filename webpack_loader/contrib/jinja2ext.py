cf4b2f81c9150b47f893b05296de4bccdb0c888e
MrP01 <peter@waldert.at>
2022-05-13 20:37:04 +0200

    add missing template functions

--- webpack_loader/contrib/jinja2ext.py
+++ webpack_loader/contrib/jinja2ext.py
@@ -1,6 +1,13 @@
 from jinja2.ext import Extension
 from jinja2.utils import pass_context
 
+from ..contrib.pages.templatetags.webpack_pages import (
+    asset_url,
+    inline_entrypoint,
+    register_entrypoint,
+    render_css,
+    render_js,
+)
 from ..templatetags.webpack_loader import render_bundle, render_entrypoint
 
 
@@ -11,5 +18,10 @@ class WebpackExtension(Extension):  # pylint: disable=abstract-method
             {
                 "render_bundle": pass_context(render_bundle),
                 "render_entrypoint": pass_context(render_entrypoint),
+                "register_entrypoint": pass_context(register_entrypoint),
+                "render_css": pass_context(render_css),
+                "render_js": pass_context(render_js),
+                "inline_entrypoint": inline_entrypoint,
+                "asset": pass_context(asset_url),
             }
         )
