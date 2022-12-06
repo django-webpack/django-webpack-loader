c18993f1c560a3969b8315c64709a77c95c681d2
Bruno Alla <alla.brunoo@gmail.com>
2022-12-06 15:12:56 +0000

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Framework :: Django',
     'Framework :: Django :: 2.0',
     'Framework :: Django :: 2.1',
