a067699e263ab99be264c9467dee82a7e803784d
Bruno Alla <alla.brunoo@gmail.com>
2023-01-27 21:27:10 +0000

    Add support for Django 4.2

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     'Framework :: Django :: 3.2',
     'Framework :: Django :: 4.0',
     'Framework :: Django :: 4.1',
+    'Framework :: Django :: 4.2',
     'Environment :: Web Environment',
     'License :: OSI Approved :: MIT License',
   ],
