95bef35fa1de50196d8e649bf2457084acd56fde
Jelmer Draaijer <info@jelmert.nl>
2023-10-31 08:21:32 +0100

    Add Python 3.12 to test matrix and add classifier

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Framework :: Django',
     'Framework :: Django :: 3.2',
     'Framework :: Django :: 4.0',
