e584c2e93026f1923f4d114bf77b7113cbff099d
luki009 <lukipalk@gmail.com>
2025-12-06 13:33:20 +0100

    add: loader warning for timeout None

--- webpack_loader/loaders.py
+++ webpack_loader/loaders.py
@@ -19,27 +19,37 @@ from .exceptions import (
 )
 
 _CROSSORIGIN_NO_REQUEST = (
-    'The crossorigin attribute might be necessary but you did not pass a '
-    'request object. django_webpack_loader needs a request object to be able '
-    'to know when to emit the crossorigin attribute on link and script tags. '
-    'Chunk name: {chunk_name}')
+    "The crossorigin attribute might be necessary but you did not pass a "
+    "request object. django_webpack_loader needs a request object to be able "
+    "to know when to emit the crossorigin attribute on link and script tags. "
+    "Chunk name: {chunk_name}"
+)
 _CROSSORIGIN_NO_HOST = (
     'You have passed the request object but it does not have a "HTTP_HOST", '
-    'thus django_webpack_loader can\'t know if the crossorigin header will '
-    'be necessary or not. Chunk name: {chunk_name}')
+    "thus django_webpack_loader can't know if the crossorigin header will "
+    "be necessary or not. Chunk name: {chunk_name}"
+)
 _NONCE_NO_REQUEST = (
-    'You have enabled the adding of nonce attributes to generated tags via '
-    'django_webpack_loader, but haven\'t passed a request. '
-    'Chunk name: {chunk_name}')
+    "You have enabled the adding of nonce attributes to generated tags via "
+    "django_webpack_loader, but haven't passed a request. "
+    "Chunk name: {chunk_name}"
+)
 _NONCE_NO_CSPNONCE = (
-    'django_webpack_loader can\'t generate a nonce tag for a bundle, '
+    "django_webpack_loader can't generate a nonce tag for a bundle, "
     'because the passed request doesn\'t contain a "csp_nonce". '
-    'Chunk name: {chunk_name}')
+    "Chunk name: {chunk_name}"
+)
+
+_LOADER_POSSIBLE_LIMBO = (
+    'You are using django_webpack_loader with default timeout "None" '
+    "which can cause request to hang indefinitely. "
+    "Validate status of webpack-stats.json file if you experience infinite loading."
+)
 
 
 @lru_cache(maxsize=100)
 def _get_netloc(url: str) -> str:
-    'Return a cached netloc (host:port) for the passed `url`.'
+    "Return a cached netloc (host:port) for the passed `url`."
     return urlparse(url=url).netloc
 
 
@@ -57,7 +67,9 @@ class WebpackLoader:
         except IOError:
             raise IOError(
                 "Error reading {0}. Are you sure webpack has generated "
-                "the file and the path is correct?".format(self.config["STATS_FILE"])
+                "the file and the path is correct?".format(
+                    self.config["STATS_FILE"]
+                )
             )
 
     def get_assets(self):
@@ -69,47 +81,56 @@ class WebpackLoader:
 
     def get_asset_by_source_filename(self, name):
         files = self.get_assets()["assets"].values()
-        return next((x for x in files if x.get("sourceFilename") == name), None)
+        return next(
+            (x for x in files if x.get("sourceFilename") == name), None
+        )
 
     def _add_crossorigin(
-            self, request: Optional[HttpRequest], chunk: Dict[str, str],
-            integrity: str, attrs_l: str) -> str:
-        'Return an added `crossorigin` attribute if necessary.'
+        self,
+        request: Optional[HttpRequest],
+        chunk: Dict[str, str],
+        integrity: str,
+        attrs_l: str,
+    ) -> str:
+        "Return an added `crossorigin` attribute if necessary."
         def_value = f' integrity="{integrity}" '
         if not request:
-            message = _CROSSORIGIN_NO_REQUEST.format(chunk_name=chunk['name'])
+            message = _CROSSORIGIN_NO_REQUEST.format(chunk_name=chunk["name"])
             warn(message=message, category=RuntimeWarning)
             return def_value
-        if 'crossorigin' in attrs_l:
+        if "crossorigin" in attrs_l:
             return def_value
-        host: Optional[str] = request.META.get('HTTP_HOST')
+        host: Optional[str] = request.META.get("HTTP_HOST")
         if not host:
-            message = _CROSSORIGIN_NO_HOST.format(chunk_name=chunk['name'])
+            message = _CROSSORIGIN_NO_HOST.format(chunk_name=chunk["name"])
             warn(message=message, category=RuntimeWarning)
             return def_value
-        netloc = _get_netloc(url=chunk['url'])
-        if netloc == '' or netloc == host:
+        netloc = _get_netloc(url=chunk["url"])
+        if netloc == "" or netloc == host:
             # Crossorigin not necessary
             return def_value
-        cfgval: str = self.config.get('CROSSORIGIN')
-        if cfgval == '':
-            return f'{def_value}crossorigin '
+        cfgval: str = self.config.get("CROSSORIGIN")
+        if cfgval == "":
+            return f"{def_value}crossorigin "
         return f'{def_value}crossorigin="{cfgval}" '
 
     def get_integrity_attr(
-            self, chunk: Dict[str, str], request: Optional[HttpRequest],
-            attrs_l: str) -> str:
-        if not self.config.get('INTEGRITY'):
+        self,
+        chunk: Dict[str, str],
+        request: Optional[HttpRequest],
+        attrs_l: str,
+    ) -> str:
+        if not self.config.get("INTEGRITY"):
             # Crossorigin only necessary when integrity is used
-            return ' '
+            return " "
 
-        integrity = chunk.get('integrity')
+        integrity = chunk.get("integrity")
         if not integrity:
             raise WebpackLoaderBadStatsError(
-                'The stats file does not contain valid data: INTEGRITY is set '
+                "The stats file does not contain valid data: INTEGRITY is set "
                 'to True, but chunk does not contain "integrity" key. Maybe '
-                'you forgot to add integrity: true in your '
-                'BundleTrackerPlugin configuration?'
+                "you forgot to add integrity: true in your "
+                "BundleTrackerPlugin configuration?"
             )
         return self._add_crossorigin(
             request=request,
@@ -118,28 +139,32 @@ class WebpackLoader:
             attrs_l=attrs_l,
         )
 
-    def get_nonce_attr(self, chunk: Dict[str, str], request: Optional[HttpRequest], attrs: str) -> str:
-        'Return an added nonce for CSP when available.'
-        if not self.config.get('CSP_NONCE'):
-            return ''
+    def get_nonce_attr(
+        self, chunk: Dict[str, str], request: Optional[HttpRequest], attrs: str
+    ) -> str:
+        "Return an added nonce for CSP when available."
+        if not self.config.get("CSP_NONCE"):
+            return ""
         if request is None:
-            message = _NONCE_NO_REQUEST.format(chunk_name=chunk['name'])
+            message = _NONCE_NO_REQUEST.format(chunk_name=chunk["name"])
             warn(message=message, category=RuntimeWarning)
-            return ''
-        nonce = getattr(request, 'csp_nonce', None)
+            return ""
+        nonce = getattr(request, "csp_nonce", None)
         if nonce is None:
-            message = _NONCE_NO_CSPNONCE.format(chunk_name=chunk['name'])
+            message = _NONCE_NO_CSPNONCE.format(chunk_name=chunk["name"])
             warn(message=message, category=RuntimeWarning)
-            return ''
-        if 'nonce=' in attrs.lower():
-            return ''
+            return ""
+        if "nonce=" in attrs.lower():
+            return ""
         return f'nonce="{nonce}" '
 
     def filter_chunks(self, chunks):
         filtered_chunks = []
 
         for chunk in chunks:
-            ignore = any(regex.match(chunk) for regex in self.config["ignores"])
+            ignore = any(
+                regex.match(chunk) for regex in self.config["ignores"]
+            )
             if not ignore:
                 filtered_chunks.append(chunk)
 
@@ -187,6 +212,10 @@ class WebpackLoader:
                 time.sleep(self.config["POLL_INTERVAL"])
                 if timeout and (time.time() - timeout > start):
                     timed_out = True
+                if not timeout:
+                    warn(
+                        message=_LOADER_POSSIBLE_LIMBO, category=RuntimeWarning
+                    )
                 assets = self.get_assets()
 
             if timed_out:
